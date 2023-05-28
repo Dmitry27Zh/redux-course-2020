341284077dc32fba240c3a45dbf5939673ca3a22
Dmitry <dmitry27zh@gmail.com>
2023-05-28 13:23:06 +0300

    Add common state and mutation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 import './styles.css'
+
+let counter = 0
+let currentTheme = 'light'
+const $counter = document.getElementById('counter')
+
+const $addBtn = document.getElementById('add')
+const $subBtn = document.getElementById('sub')
+const $asyncBtn = document.getElementById('async')
+const $themeBtn = document.getElementById('theme')
+
+const renderCounter = () => {
+  $counter.textContent = counter
+}
+
+const renderTheme = () => {
+  document.body.classList.toggle('light', currentTheme === 'light')
+  document.body.classList.toggle('dark', currentTheme === 'dark')
+}
+
+$addBtn.addEventListener('click', () => {
+  counter++
+  renderCounter()
+})
+
+$subBtn.addEventListener('click', () => {
+  counter--
+  renderCounter()
+})
+
+$asyncBtn.addEventListener('click', () => {
+  counter++
+  renderCounter()
+})
+
+$themeBtn.addEventListener('click', () => {
+  currentTheme = currentTheme === 'light' ? 'dark' : 'light'
+  renderTheme()
+})
+
+renderCounter()
+renderTheme()
